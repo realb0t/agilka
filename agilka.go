4af7934b5bc8b1ce1c0f856d2cbc3a853fedc9e5
realbot <realbot@w3soft.ru>
2015-04-10 08:05:03 +0300

    Добавил парсинг мулти флагов

--- agilka.go
+++ agilka.go
@@ -4,11 +4,24 @@ import (
   //"fmt"
   //"encoding/json"
   "os"
+  "fmt"
+  "strings"
   //"github.com/deiwin/interact"
   "github.com/codegangsta/cli"
   "github.com/realb0t/agilka/obj"
   //"strings"
 )
+
+func parseFieldsFlags(c *cli.Context) map[string]string {
+  flags := c.StringSlice("field")
+  fields := make(map[string]string)
+  for _, fieldVal := range flags {
+    f := strings.Split(fieldVal, "=")
+    fields[f[0]] = f[1]
+  }
+
+  return fields
+}
 
 func main() {
   obj.Init()
@@ -19,7 +32,15 @@ func main() {
       Name:    "new",
       Aliases: []string{"a"},
       Usage:   "add a task to the list",
+      Flags:   []cli.Flag{
+        cli.StringSliceFlag{
+          Name: "field, f",
+          Value: &cli.StringSlice{},
+          Usage: "Fields defenitions",
+        },
+      },
       Action:  func(c *cli.Context) {
+        fmt.Println("flags", parseFieldsFlags(c))
         objName := c.Args().First()
         o := obj.CreateByName(objName)
         println("added object: ", o.Marshal())
