14b0d5998f7582ac3fb81ad6b1b766df14a525d3
realbot <realbot@w3soft.ru>
2015-04-16 23:51:42 +0300

    Поправил тест для операций

--- operation/operation_test.go
+++ operation/operation_test.go
@@ -1,8 +1,7 @@
 package operation
 
 import "testing"
-import "github.com/realb0t/agilka/task"
 
 func TestNewOperation(t *testing.T) {
-  _ = NewOperation(task.NewTask(false))
+  _ = NewOperation(nil)
 }
